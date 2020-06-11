92d6dc4743f523c2281e6d68d1c29a80c002af5a
Soma Ikeda <ikeda.s@sprobe.com>
2020-06-11 15:18:12 +0800

    Updates

---
+++ precache-manifest.76155fa831fe6588d2996dc2581806da.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800352eae45bc02c9d522c70cd5f16a1",
+    "url": "/camera-test/index.html"
+  },
+  {
+    "revision": "852041ab48c76e77065e",
+    "url": "/camera-test/static/css/2.0939e70c.chunk.css"
+  },
+  {
+    "revision": "17f0eb4036d264dfd878",
+    "url": "/camera-test/static/css/main.5b605b45.chunk.css"
+  },
+  {
+    "revision": "852041ab48c76e77065e",
+    "url": "/camera-test/static/js/2.bf288209.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/camera-test/static/js/2.bf288209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f0eb4036d264dfd878",
+    "url": "/camera-test/static/js/main.8976e19f.chunk.js"
+  },
+  {
+    "revision": "05505996601ff492ad76",
+    "url": "/camera-test/static/js/runtime-main.7bc20881.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/camera-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
