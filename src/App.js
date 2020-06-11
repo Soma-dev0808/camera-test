d1a4ae206c8795651984efd6839f59b86a9bc049
Soma Ikeda <ikeda.s@sprobe.com>
2020-06-11 15:12:48 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CameraComponent from "./ChildComponents/CameraComponent";
+
+function App() {
+  return <CameraComponent />;
+}
+
+export default App;
---
+++ src/ChildComponents/CameraComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../index.css";
+import Camera from "react-html5-camera-photo";
+import "react-html5-camera-photo/build/css/index.css";
+
+function CameraComponent() {
+  function handleTakePhoto(dataUri) {
+    console.log(dataUri);
+  }
+  return (
+    <div className="wrapper">
+      <div className="child-items">
+        <h3>Hello</h3>
+      </div>
+      <div className="child-items fl-column">
+        <h3>Camera Button</h3>
+        <h3>Click button to open it</h3>
+        <Camera
+          onTakePhoto={(dataUri) => {
+            handleTakePhoto(dataUri);
+          }}
+        ></Camera>
+      </div>
+      <div className="child-items">
+        <h3>Hello</h3>
+      </div>
+    </div>
+  );
+}
+
+export default CameraComponent;
